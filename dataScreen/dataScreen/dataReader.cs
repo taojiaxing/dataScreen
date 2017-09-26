5a95771f768ab290a4dfea108656c2047c9dc99e
ts0118 <taojiaxing06@gmail.com>
2017-09-26 19:39:49 +0800

    creat project

---
+++ dataScreen/dataScreen/dataReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataScreen
+{
+    class dataReader
+    {
+        private String dataSouce;
+        dataReader(String _dataSouce)
+        {
+            this.dataSouce = _dataSouce;
+        }
+        String Read()
+        {
+            String data;
+
+            return data;
+        }
+    }
+}
