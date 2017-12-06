d95216c1c22780ed97311b9b9ca2ec8582cfdced
ts0118 <taojiaxing06@gmail.com>
2017-12-06 11:06:54 +0800

    自动截图可通过修改配置文件实现设置

---
+++ ScreenShot/ScreenShot/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Text;
+
+namespace ScreenShot
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /* Process p = Process.Start("E:/Thinkgeo/MapSuite9SamplesUpgrade/Tools/tool/DisplayWmsRasterLayerSample-ForWinForms/DisplayWmsRasterLayer/bin/Debug/Map Suite Desktop for WinForms App1.exe");
+             p.WaitForExit();*/
+            int fail = 0;
+            int Success = 0;
+            String selectResultPath = ConfigurationManager.AppSettings["selectResultPath"].ToString();
+            String screenshotPath = ConfigurationManager.AppSettings["screenshotPath"].ToString();
+            if (File.Exists(selectResultPath))
+            {
+                if (!Directory.Exists(screenshotPath))
+                {
+                    Directory.CreateDirectory(screenshotPath);
+                }
+                DirectoryInfo directory = new DirectoryInfo(screenshotPath);
+                if (directory.Exists)
+                {
+                    StreamReader sr = new StreamReader(selectResultPath, Encoding.Default);
+                    String line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        String[] Name = line.Split('-');
+                        String fileName = Name[0].Replace("Sample", "");
+                        String Directory = selectResultPath.Replace("\\", "/");
+                        String workingDirectory = Directory.Replace("selectResult.txt", "") + "Project/" + line + "/" + fileName + "/bin/Debug";
+                        Start start = new Start();
+                        DirectoryInfo directoryInfo = new DirectoryInfo(workingDirectory);
+                        StringBuilder stringBuilder = new StringBuilder();
+                        foreach (FileInfo info in directoryInfo.GetFiles("*.exe"))
+                        {
+                            stringBuilder.Append(info.Name);
+                        }
+                        stringBuilder.Replace(".exeThinkGeo.MapSuite.ProductCenter.exe", "");
+                        Boolean s = start.start(stringBuilder.ToString(), workingDirectory, screenshotPath);
+                        if (s)
+                        {
+                            Success += 1;
+                        }
+                        else
+                        {
+                            fail += 1;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(value: "screenShot文件不存在");
+                }
+            }
+            else
+            {
+                Console.WriteLine("selectResult.txt不存在");
+            }
+            Console.WriteLine("成功截图：" + Success + "个项目");
+            Console.WriteLine("失败    :" + fail + "个项目");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ ScreenShot/ScreenShot/Start.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace ScreenShot
+{
+    internal class Start
+    {
+        public Boolean start(String fileName, String workingDirectory, String screenshotPath)
+        {
+            ProcessStartInfo Info = new ProcessStartInfo
+            {
+                FileName = fileName + ".exe", //"Map Suite Desktop for WinForms App1.exe",
+                WorkingDirectory = workingDirectory //"E:/Thinkgeo/MapSuite9SamplesUpgrade/Tools/tool/DisplayWmsRasterLayerSample-ForWinForms/DisplayWmsRasterLayer/bin/Debug/"
+            };
+            Process Proc;
+            try
+            {
+                Proc = Process.Start(Info);
+            }
+            catch (System.ComponentModel.Win32Exception e)
+            {
+                Console.WriteLine("系统找不到指定的程序文件。\r{0}", e);
+                return false;
+            }
+
+            //打印出外部程序的开始执行时间
+            Console.WriteLine(fileName + "截图开始执行时间：{0}", Proc.StartTime);
+
+            //等待3秒钟
+            Proc.WaitForExit(10000);
+            ScreenShot s = new ScreenShot();
+            Image img = s.CaptureWindow(Proc.MainWindowHandle);
+            img.Save(screenshotPath + "/" + fileName + ".png");
+            //如果这个外部程序没有结束运行则对其强行终止
+            if (Proc.HasExited == false)
+            {
+                Console.WriteLine("由主程序强行终止外部程序的运行！");
+                Proc.Kill();
+            }
+            else
+            {
+                Console.WriteLine("由外部程序正常退出！");
+            }
+            if (img.Width < 10)
+            {
+                Console.WriteLine(fileName + "截图失败");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine(fileName + "截图成功");
+                return true;
+            }
+            Console.WriteLine("截图结束运行时间：{0}", Proc.ExitTime);
+            Console.WriteLine("");
+        }
+    }
+}
